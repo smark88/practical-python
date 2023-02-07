3c3af4aab657a390331367997eb5de511348016e
smark88 <msarcevicz@influxdata.com>
2023-02-06 23:01:58 -0500

    restructure

--- Work/string-num-manipulation.py
+++ Work/string-num-manipulation.py
@@ -1,18 +1,52 @@
+string1 = "Hello"
+string2 = "World"
+
 def concatenate_strings(string1, string2):
     return string1 + " " + string2
+
+concatenated = concatenate_strings(string1, string2)
+print("Concatenated:", concatenated)
+'''
+Output:
+Concatenated: Hello World
+'''
 
 def slice_string(string, start, end):
     return string[start:end]
 
+string = "Hello World"
+sliced = slice_string(string, 0, 5)
+print("Sliced:", sliced)
+'''
+Output:
+Sliced: Hello
+'''
 def replace_string(string, old, new):
     return string.replace(old, new)
 
+replaced = replace_string(string, "Hello", "Hi")
+print("Replaced:", replaced)
+'''
+Output:
+Replaced: Hi World
+'''
+
 def find_substring(string, substring):
     return string.find(substring)
+found = find_substring(string, "World")
+print("Found:", found)
+'''
+Output:
+Found: 6
+'''
 
 def format_string(string, values):
     return string.format(*values)
-
+formatted = format_string("Hello, {}", ["John"])
+print("Formatted:", formatted)
+'''
+Formatted: Hello, John
+'''
 
 def permutation(string, mutation=''):
     '''
@@ -24,6 +58,19 @@ def permutation(string, mutation=''):
         newMutation = mutation + string[i]
         newString = string[0:i] + string[i+1:]
         permutation(newString, newMutation)
+print("Permutations of \"" + string1 + "\" is as below,")
+permutation('ABC')
+'''
+Output:
+Permutations of "Hello" is as below,
+ABC
+ACB
+BAC
+BCA
+CAB
+CBA
+'''
+
 
 def reverse(text):
     '''
@@ -36,6 +83,12 @@ def reverse(text):
     for i in text:
         newText = i + newText
     print(f'Using for loop : {newText}')
+reverse(string1)
+'''
+Output:
+Using String slicing : olleH
+Using for loop : olleH
+'''
 
 def swapNumberBuild(a,b):
     '''
@@ -43,13 +96,26 @@ def swapNumberBuild(a,b):
     '''
     a, b = b, a
     print("Using in-build syntax : a is" , a , "& b is" , b)
+print('Swap two numbers without using third variable via build')
+swapNumberBuild(10,20)
+'''
+Output:
+Swap two numbers without using third variable via build
+Using in-build syntax : a is 20 & b is 10
+'''
 
 def swapNumberMath(a,b): 
     a = a + b
     b = a - b
     a = a - b
     print("Using arithmetic operations : a is" , a , "& b is" , b)
-
+print('Swap two numbers without using third variable via build')
+swapNumberMath(10,20)
+'''
+Output:
+Swap two numbers without using third variable via build
+Using arithmetic operations : a is 20 & b is 10
+'''
 
 def fib(n):
     '''
@@ -58,7 +124,18 @@ def fib(n):
     if n <= 1:
         return n
     return (fib(n-1) + fib(n-2))
-
+print("Fibonacci sequence : ")
+for i in range(5):
+    print(fib(i))
+'''
+Output:
+Fibonacci sequence : 
+0
+1
+1
+2
+3
+'''
 
 def sum(n):
     '''
@@ -70,7 +147,11 @@ def sum(n):
         allDigits = n // 10
         lastDigit = n % 10
         return sum(allDigits) + lastDigit
-
+print("Sum of digits :" , sum(2021))
+'''
+Output:
+Sum of digits : 5
+'''
 
 def fac(n):
     '''
@@ -81,6 +162,11 @@ def fac(n):
     else:
         print(n)
         return (n * fac(n-1))
+print("5! (5 * 4 * 3 * 2) =", fac(5))
+'''
+Output:
+5! (5 * 4 * 3 * 2) = 120
+'''
 
 import os,re
 def write_data(source, destination):
@@ -95,45 +181,6 @@ def write_data(source, destination):
             with open(source+'/'+file,'r') as f, open(destination+'/'+file,'a') as s:
                 for line in f:
                     s.write(line)
-
-# Example usage:
-string1 = "Hello"
-string2 = "World"
-concatenated = concatenate_strings(string1, string2)
-print("Concatenated:", concatenated)
-
-string = "Hello World"
-sliced = slice_string(string, 0, 5)
-print("Sliced:", sliced)
-
-replaced = replace_string(string, "Hello", "Hi")
-print("Replaced:", replaced)
-
-found = find_substring(string, "World")
-print("Found:", found)
-
-formatted = format_string("Hello, {}", ["John"])
-print("Formatted:", formatted)
-
-print("Permutations of \"" + string1 + "\" is as below,")
-permutation(string1)
-
-print('string reverse')
-reverse(string1)
-
-print('Swap two numbers without using third variable via build')
-swapNumberBuild(10,20)
-
-print('Swap two numbers without using third variable via build')
-swapNumberMath(10,20)
-
-print("Fibonacci sequence : ")
-for i in range(5):
-    print(fib(i))
-
-print("Sum of digits :" , sum(2021))
-
-print("5! (5 * 4 * 3 * 2) =", fac(5))
 
 print('copy folder A files and copy to folder C')
 write_data('FolderA','FolderC')
